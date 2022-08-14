60847162121d7c6a2dbcd1e1307ebf80768c6678
Ady <advayb2018@gmail.com>
2022-08-14 11:43:01 -0700

    Initial commit

---
+++ src/main/java/com/ady/bundlesreworked/BundleItem.java
@@ -0,0 +1,9 @@
+package com.ady.bundlesreworked;
+
+import net.minecraft.world.item.DyeableLeatherItem;
+
+public class BundleItem extends net.minecraft.world.item.BundleItem implements DyeableLeatherItem {
+    public BundleItem(Properties p_150726_) {
+        super(p_150726_);
+    }
+}
